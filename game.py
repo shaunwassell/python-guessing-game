5b1a580b41b97bcda896048609bed51d594373ab
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-07-10 17:58:27 -0400

    Informed user whether guess was too high or too low

--- game.py
+++ game.py
@@ -5,5 +5,7 @@ guess = int(input('I have selected a number between 1 and 10. Enter your guess: 
 
 if guess == number:
     print(f'You are correct! The number was {number}')
+elif guess > number:
+    print(f'Nope, you are wrong! That guess was too high')
 else:
-    print(f'Nope, you are wrong! The number was {number}')
+    print(f'Nope, you are wrong! That guess was too low')
