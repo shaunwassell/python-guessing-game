f527a7960bf24ff8b78f933f59d6e830efa08926
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-07-10 17:55:21 -0400

    Added prompt for difficulty

--- game.py
+++ game.py
@@ -1,6 +1,14 @@
 import random
 
-number = random.randint(1, 10)
+mode = input('Select game mode (E/M/H): ')
+
+if mode == 'E':
+    number = random.randint(1, 5)
+elif mode == 'M':
+    number = random.randint(1, 10)
+else:
+    number = random.randint(1, 20)
+
 guess = int(input('I have selected a number between 1 and 10. Enter your guess: '))
 
 if guess == number:
