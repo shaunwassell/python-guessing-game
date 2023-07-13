28e86108297db1583dfbf1dbb155b2893c38cee7
shaunwassell <shaun.wassell@cbtnuggets.com>
2023-07-13 15:37:12 -0400

    Made messages less formal

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
 import random
 
 number = random.randint(1, 10)
-guess = int(input('I have selected a number between 1 and 10. Enter your guess: '))
+guess = int(input('I have selected a number between 1 and 10. What do you think it is???: '))
 
 while guess != number:
     print(f'Nope, you are wrong!')
     guess = int(input('Enter another guess: '))
 
-print(f'You are correct! The number was {number}')
+print(f'Yayyyyyy! The number was {number}')
